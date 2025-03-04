ab6efc3ab00da5cff4ab526802791d85acc53e9c
KurniaDuta <dutaka2945@gmail.com>
2025-03-04 09:05:58 +0700

    Jobsheet 4- Praktikum 2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ use Psy\Readline\Userland;
 class UserController extends Controller
 {   
     public function index() {
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
         return view('user', ['data' => $user]);
     }
 }
