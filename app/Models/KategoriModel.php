9ef3b15d8a7e39320466df43c1351f4bf4eb56ed
KurniaDuta <dutaka2945@gmail.com>
2025-03-11 21:43:01 +0700

    J5 - Tugas - Tabel m_barang

--- app/Models/KategoriModel.php
+++ app/Models/KategoriModel.php
@@ -12,4 +12,9 @@ class KategoriModel extends Model
     protected $table = 'm_kategori';
     protected $primaryKey = 'kategori_id';
     protected $fillable = ['kategori_kode', 'kategori_nama'];
+
+    public function barang()
+    {
+        return $this->hasMany(BarangModel::class, 'kategori_id', 'kategori_id');
+    }
 }
