16535718cb7047ee0ed3d83f9e4aed6221841435
KurniaDuta <dutaka2945@gmail.com>
2025-03-17 22:13:32 +0700

    J6 - Praktikum 3 Modal Ajax Hapus Data User

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::prefix('user')->group(function () {
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); 
     Route::delete('/{id}', [UserController::class, 'destroy']);
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
 });
 
 
