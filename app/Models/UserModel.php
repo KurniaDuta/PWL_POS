27811d777fde1e44ef0bcc74c85cc1b62e244f4d
KurniaDuta <dutaka2945@gmail.com>
2025-03-25 04:39:37 +0700

    J7 - Praktikum 1 Implementasi Authentication

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
@@ -14,6 +15,10 @@ class UserModel extends Model
     protected $primaryKey = 'user_id';
 
     protected $fillable = ['username', 'nama', 'password', 'level_id'];
+
+    protected $hidden = ['password'];
+
+    protected $casts = ['password' => 'hashed'];
 
     public function level(): BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
