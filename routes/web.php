02e83927eb9c834dfb3a3b47d035d0c93a75edff
KurniaDuta <dutaka2945@gmail.com>
2025-03-25 20:33:48 +0700

    J7 - Tugas 4 Implementasi Form Registrasi

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ Route::pattern('id', '[0-9]+');
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/postlogin', [AuthController::class, 'postlogin']);
 Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth');
+Route::get('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'postRegister']);
 
 Route::middleware('auth')->group(function () {
 
